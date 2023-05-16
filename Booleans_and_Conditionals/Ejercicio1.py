cdc01d1ada2ae9125ab4940a0197296c808c441c
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-15 22:28:21 -0600

    Create Ejercicio1.py

---
+++ Booleans_and_Conditionals/Ejercicio1.py
@@ -0,0 +1,12 @@
+# Your code goes here. Define a function called 'sign'
+def sign(n):
+    if (n < 0):
+        return -1
+    elif (n > 0):
+        return 1
+    elif (n == 0):
+        return 0
+                
+
+# Check your answer
+q1.check()
