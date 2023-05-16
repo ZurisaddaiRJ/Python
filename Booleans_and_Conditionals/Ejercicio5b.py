1287afd3eca4b31cbf48f212316ab13049911ff3
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-15 22:32:34 -0600

    Create Ejercicio5b.py

---
+++ Booleans_and_Conditionals/Ejercicio5b.py
@@ -0,0 +1,8 @@
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    return not ketchup and not onion and not mustard
+
+
+# Check your answer
+q5.b.check()
