5d26e5693a2b045bc969cbdd93293b86664c5c7e
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-15 22:17:44 -0600

    Create Ejercicio1.py

---
+++ hello-python /Ejercicio1.py
@@ -0,0 +1,13 @@
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+____
+radius = diameter / 2
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+____
+area = pi * (radius**2)
+
+# Check your answer
+q1.check()
