d898133cb3cd42c78308adba27d313a397d42423
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-15 22:16:50 -0600

    Create Ejercicio0.py

---
+++ hello-python /Ejercicio0.py
@@ -0,0 +1,7 @@
+# create a variable called color with an appropriate value on the line below
+# (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
+____
+color = 'blue'
+
+# Check your answer
+q0.check()
