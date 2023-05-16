d43e74f7da6359e950fd01af16119e48d75cc80d
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-15 22:31:40 -0600

    Create Ejercicio4.py

---
+++ Booleans_and_Conditionals/Ejercicio4.py
@@ -0,0 +1,10 @@
+def is_negative(number):
+    if number < 0:
+        return True
+    else:
+        return False
+
+def concise_is_negative(number):
+    return concise_is_negative and (number < 0) 
+# Check your answer
+q4.check()
