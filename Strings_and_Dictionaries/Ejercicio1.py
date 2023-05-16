5b069abd7ce2a3e4e709c6c920246f1b82dad849
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-15 22:42:43 -0600

    Create Ejercicio1.py

---
+++ Strings_and_Dictionaries/Ejercicio1.py
@@ -0,0 +1,7 @@
+def is_valid_zip(zip_code):
+    """Returns whether the input string is a valid (5 digit) zip code
+    """
+    return len(zip_code)== 5 and zip_code.isdigit()
+
+# Check your answer
+q1.check()
